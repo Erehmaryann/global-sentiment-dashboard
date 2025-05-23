fbb2f7b2bed11e5b4e7fe8b406167f251f0af747
Erehmaryann <edwardmaryann09@gmail.com>
2025-05-23 15:28:45 +0100

    Configure Vite for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://github.com/Erehmaryann/global-sentiment-dashboard",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
